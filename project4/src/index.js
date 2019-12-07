51a849e0a5052a7e1c1d46f05131e0aaadd59a1a
Neil Champakara <nchamp3@uic.edu>
2019-12-07 20:03:31 +0000

    Showed past moves

--- project4/src/index.js
+++ project4/src/index.js
@@ -24,7 +24,6 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -77,6 +76,17 @@ class Game extends React.Component {
       const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+      
+      const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -96,7 +106,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
